cb408cc6d5e72b9e4b778070d2c192dbbcef341b
LibertyForUs <45527765+LibertyForUs@users.noreply.github.com>
2023-07-11 15:01:29 -0700

    New cool ui system
    Started using the ui builder tool

--- Assets/Scripts/playerUIController.cs
+++ Assets/Scripts/playerUIController.cs
@@ -7,87 +7,104 @@ using UnityEngine.UIElements;
 
 public class playerUIController : MonoBehaviour
 {
-	public playerMovement playerStuff;
-	public enemyScripting enemyStuff;
+    public playerMovement playerStuff;
+    public enemyScripting enemyStuff;
 
-
-
-	public Label playerText;
-	public Label enemyText;
+    public Label playerText;
+    public Label enemyText;
 
     private void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
 
-		playerText = root.Q<Label>("PlayerDialogue");
-		enemyText = root.Q<Label>("EnemyDialogue");
+        playerText = root.Q<Label>("PlayerDialogue");
+        enemyText = root.Q<Label>("EnemyDialogue");
     }
 
     private IEnumerator dialogue(dialogueParsing.dialogueLine[] lines)
-	{
-		foreach (dialogueParsing.dialogueLine line in lines)
-		{
+    {
+        foreach (dialogueParsing.dialogueLine line in lines)
+        {
             if (line.Subject)
-			{
+            {
                 for (int i = 1; i <= line.dialogueText.Length; i++)
                 {
                     playerText.text = line.dialogueText.Substring(0, i);
-                    
 
                     yield return new WaitForSeconds(.02f);
                 }
 
                 playerText.text = line.dialogueText;
             }
-			else
-			{
+            else
+            {
                 for (int i = 1; i <= line.dialogueText.Length; i++)
                 {
                     enemyText.text = line.dialogueText.Substring(0, i);
-					
 
                     yield return new WaitForSeconds(.02f);
                 }
-				enemyText.text = line.dialogueText;
+                enemyText.text = line.dialogueText;
             }
 
-			
-			
-			yield return new WaitUntil(() => Input.GetButtonDown("Submit"));
+            yield return new WaitUntil(() => Input.GetButtonDown("Submit"));
 
-			enemyText.text = "";
-			playerText.text = "";
-		}
-		stopDialogue();
-	}
+            enemyText.text = "";
+            playerText.text = "";
+        }
+        stopDialogue();
+    }
 
-	
-
-
-	public void startDialogue(dialogueParsing.Dialogue dialogueRoot)
-	{
-		print("f");
-		DOTween.To(() => playerText.transform.position.x, x => playerText.transform.position = new Vector3(x, playerText.transform.position.y), 0, 1).SetEase(Ease.Linear);
-        DOTween.To(() => enemyText.transform.position.x, x => enemyText.transform.position = new Vector3(x, enemyText.transform.position.y), 0, 1).SetEase(Ease.Linear);
-
+    public void startDialogue(dialogueParsing.Dialogue dialogueRoot)
+    {
+        print("f");
+        DOTween
+            .To(
+                () => playerText.transform.position.x,
+                x =>
+                    playerText.transform.position = new Vector3(x, playerText.transform.position.y),
+                0,
+                1
+            )
+            .SetEase(Ease.Linear);
+        DOTween
+            .To(
+                () => enemyText.transform.position.x,
+                x => enemyText.transform.position = new Vector3(x, enemyText.transform.position.y),
+                0,
+                1
+            )
+            .SetEase(Ease.Linear);
 
         foreach (dialogueParsing.dialogueData data in dialogueRoot.cutscene_Dialogue)
-		{
-			
-			StartCoroutine(dialogue(data.Start));
-			
-		}
+        {
+            StartCoroutine(dialogue(data.Start));
+        }
 
         //stopDialogue();
     }
 
-	private void stopDialogue()
-	{
-		playerStuff.inDialogue = false;
-        DOTween.To(() => playerText.transform.position.x, x => playerText.transform.position = new Vector3(x, playerText.transform.position.y), 113, 1).SetEase(Ease.Linear);
-        DOTween.To(() => enemyText.transform.position.x, x => enemyText.transform.position = new Vector3(x, enemyText.transform.position.y), -113, 1).SetEase(Ease.Linear);
+    private void stopDialogue()
+    {
+        playerStuff.inDialogue = false;
+        DOTween
+            .To(
+                () => playerText.transform.position.x,
+                x =>
+                    playerText.transform.position = new Vector3(x, playerText.transform.position.y),
+                300,
+                1
+            )
+            .SetEase(Ease.Linear);
+        DOTween
+            .To(
+                () => enemyText.transform.position.x,
+                x => enemyText.transform.position = new Vector3(x, enemyText.transform.position.y),
+                -300,
+                1
+            )
+            .SetEase(Ease.Linear);
 
         enemyStuff.continualizeFight();
-	}
-
+    }
 }
