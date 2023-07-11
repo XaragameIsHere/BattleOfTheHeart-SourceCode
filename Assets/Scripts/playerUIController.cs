68fd604609aa8e54a7f74efc9f1e6674d7657822
LibertyForUs <45527765+LibertyForUs@users.noreply.github.com>
2023-07-11 14:45:08 -0700

    New cool ui system
    Started using the

--- Assets/Scripts/playerUIController.cs
+++ Assets/Scripts/playerUIController.cs
@@ -7,6 +7,7 @@ using DentedPixel;
 
 public class playerUIController : MonoBehaviour
 {
+<<<<<<< Updated upstream
 	public playerMovement playerStuff;
 
 	public Image dialoguePlayer;
@@ -19,34 +20,51 @@ public class playerUIController : MonoBehaviour
 	{
 		foreach (dialogueParsing.dialogueLine line in lines)
 		{
+=======
+    public playerMovement playerStuff;
+    public enemyScripting enemyStuff;
+
+    public Label playerText;
+    public Label enemyText;
+
+    private void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        playerText = root.Q<Label>("PlayerDialogue");
+        enemyText = root.Q<Label>("EnemyDialogue");
+    }
+
+    private IEnumerator dialogue(dialogueParsing.dialogueLine[] lines)
+    {
+        foreach (dialogueParsing.dialogueLine line in lines)
+        {
+>>>>>>> Stashed changes
             if (line.Subject)
-			{
+            {
                 for (int i = 1; i <= line.dialogueText.Length; i++)
                 {
                     playerText.text = line.dialogueText.Substring(0, i);
-                    
 
                     yield return new WaitForSeconds(.02f);
                 }
 
                 playerText.text = line.dialogueText;
             }
-			else
-			{
+            else
+            {
                 for (int i = 1; i <= line.dialogueText.Length; i++)
                 {
                     enemyText.text = line.dialogueText.Substring(0, i);
-					
 
                     yield return new WaitForSeconds(.02f);
                 }
-				enemyText.text = line.dialogueText;
+                enemyText.text = line.dialogueText;
             }
 
-			
-			
-			yield return new WaitUntil(() => Input.GetButtonDown("Submit"));
+            yield return new WaitUntil(() => Input.GetButtonDown("Submit"));
 
+<<<<<<< Updated upstream
 			enemyText.text = "";
 			playerText.text = "";
 		}
@@ -81,4 +99,64 @@ public class playerUIController : MonoBehaviour
 		
 	}
 
+=======
+            enemyText.text = "";
+            playerText.text = "";
+        }
+        stopDialogue();
+    }
+
+    public void startDialogue(dialogueParsing.Dialogue dialogueRoot)
+    {
+        print("f");
+        DOTween
+            .To(
+                () => playerText.transform.position.x,
+                x =>
+                    playerText.transform.position = new Vector3(x, playerText.transform.position.y),
+                0,
+                1
+            )
+            .SetEase(Ease.Linear);
+        DOTween
+            .To(
+                () => enemyText.transform.position.x,
+                x => enemyText.transform.position = new Vector3(x, enemyText.transform.position.y),
+                0,
+                1
+            )
+            .SetEase(Ease.Linear);
+
+        foreach (dialogueParsing.dialogueData data in dialogueRoot.cutscene_Dialogue)
+        {
+            StartCoroutine(dialogue(data.Start));
+        }
+
+        //stopDialogue();
+    }
+
+    private void stopDialogue()
+    {
+        playerStuff.inDialogue = false;
+        DOTween
+            .To(
+                () => playerText.transform.position.x,
+                x =>
+                    playerText.transform.position = new Vector3(x, playerText.transform.position.y),
+                400,
+                1
+            )
+            .SetEase(Ease.Linear);
+        DOTween
+            .To(
+                () => enemyText.transform.position.x,
+                x => enemyText.transform.position = new Vector3(x, enemyText.transform.position.y),
+                -400,
+                1
+            )
+            .SetEase(Ease.Linear);
+
+        enemyStuff.continualizeFight();
+    }
+>>>>>>> Stashed changes
 }
